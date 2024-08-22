bed51a9d4728634794f1eac2971aa43add58bbf5
whitebird1016 <takus.superdev@gmail.com>
2024-08-22 11:13:55 +0900

    add astro setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://creativetimofficial.github.io",
-  base: "/astro-launch-ui-pro/",
+  base: "/",
   image: {
     domains: ["astro.build"],
-  }
+  },
+  output: "server",
+  adapter: vercelServerless(),
 });
